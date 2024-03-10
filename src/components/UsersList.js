9db4d299239e35315dfb2e4930a3280636c25cc4
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-03-10 08:48:09 +0300

    creating button spin animate

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -36,7 +36,7 @@ const UsersList = () => {
         setCreatingUserError(err);
       })
       .finally(() => {
-        setCreatingUserError(false);
+        setIsCreatingUser(false);
       });
   };
 
@@ -62,7 +62,12 @@ const UsersList = () => {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl ">Users</h1>
-        <Button onClick={handleUserAdd}>+ Add User</Button>
+
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
+
+        {creatingUserError && <div>Error creating user</div>}
       </div>
       {renderedUsers}
     </div>
