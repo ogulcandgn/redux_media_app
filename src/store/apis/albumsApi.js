6e4f47ece0b29f2850a1362afa44553fe6b399ab
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-03-13 16:41:35 +0300

    creating endpoints

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,3 +1,20 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const albumsApi = createApi({});
+const albumsApi = createApi({
+  //* albums key'i herhangi bir şey olabilir, burada diğer yazılımcılara anlamlı gelmesi için albums key'ini olusturduk
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://localhost:3005" }),
+  endpoints: (builder) => {
+    fetchAlbums: builder.query({
+      query: (user) => {
+        return {
+          url: "/albums",
+          params: {
+            userId: user.id,
+          },
+          method: "GET",
+        };
+      },
+    });
+  },
+});
