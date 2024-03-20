c132472a764751c6dbebdb40f6046d3c600ae196
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-03-20 16:11:59 +0300

    create handleAddAlbum button

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -37,5 +37,5 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
