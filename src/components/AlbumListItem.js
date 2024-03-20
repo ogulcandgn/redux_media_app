bf03ede8bdaa4fa511455f26e05dfec7cdf7a449
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-03-20 19:04:12 +0300

    create delete albumList icons

--- src/components/AlbumListItem.js
+++ src/components/AlbumListItem.js
@@ -1,8 +1,16 @@
 import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 function AlbumListItem({ album }) {
-  const header = <div>{album.title}</div>;
+  const header = (
+    <div className="flex justify-center items-center">
+      <Button className="mr-2">
+        <FaRegTrashCan />
+      </Button>
+      {album.title}
+    </div>
+  );
   return (
     <ExpandablePanel key={album.id} header={header}>
       List of photos in the album
